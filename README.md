bc83d39c5f930419eeaefc98cf2430e5c4e92aa7
SallesThe <manodada26@gmail.com>
2022-05-22 21:37:43 -0300

    Alocando projeto no GITHUB.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Atenção :exclamation:
+
+### O programa tem como objetivo mostrar para o usuário quanto ele ganha por dia e por hora. 
+
+### Mais um projetinho desenvolvido por mim !
---
+++ SalarioPorHora/SalarioPorHora/SalarioPorHoraTrabalhada.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioPorHora
+{
+    public class SalarioPorHoraTrabalhada
+    {
+        public static void Iniciar()
+        {
+            LerDados();
+        }
+
+        public static void LerDados()
+        {
+            double salario;
+            int horasTrabalhadasDia;
+            double totalSalarioDia;
+            double totalSalarioHora;
+            int mesTrabalhado = 22;
+
+            Console.WriteLine("Informe seu salário: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Informe a suas horas trabalhadas por dia: ");
+            horasTrabalhadasDia = Convert.ToInt32(Console.ReadLine());
+
+            totalSalarioDia = salario / mesTrabalhado;
+
+            totalSalarioHora = totalSalarioDia / horasTrabalhadasDia;
+
+            Console.WriteLine($"Você recebe por dia: {totalSalarioDia}");
+
+            Console.WriteLine($"E por hora: {totalSalarioHora}");
+        }
+    }
+}
